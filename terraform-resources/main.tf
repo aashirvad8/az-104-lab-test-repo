06b6b75211be190cd7af045ce353e30d1b4b8ffb
aashirvad8 <aashirvad8@gmail.com>
2025-10-27 01:36:09 +0530

    Azure VM Structured Code

---
+++ terraform-resources/main.tf
@@ -0,0 +1,67 @@
+# -------------------------------
+# Resource Group
+# -------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_Name
+  location = var.location
+}
+
+# -------------------------------
+# Backend Storage (TF State)
+# -------------------------------
+resource "azurerm_resource_group" "backend_rg" {
+  name     = var.backend_rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "backend_sa" {
+  name                     = var.backend_sa_name
+  resource_group_name      = azurerm_resource_group.backend_rg.name
+  location                 = azurerm_resource_group.backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  kind                     = "StorageV2"
+}
+
+resource "azurerm_storage_container" "backend_container" {
+  name                  = var.backend_container_name
+  storage_account_name  = azurerm_storage_account.backend_sa.name
+  container_access_type = "private"
+}
+
+# -------------------------------
+# VNET Module
+# -------------------------------
+module "vnet01" {
+  source             = "../terraform-modules/network"
+  vnet_Name          = var.vnet_Name
+  rg_Name            = azurerm_resource_group.rg.name
+  location           = azurerm_resource_group.rg.location
+  vnet_Address       = var.vnet_Address
+  subnet_NameList    = var.subnet_NameList
+  subnet_AddressList = var.subnet_AddressList
+}
+
+# -------------------------------
+# Windows VM Module
+# -------------------------------
+module "winvm" {
+  source               = "../terraform-modules/virtual_machine"
+  vm_pip               = var.vm_pip
+  rg_Name              = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  pip_allocation       = var.pip_allocation
+  vm_nic               = var.vm_nic
+  ip_configuration     = var.ip_configuration
+  vm_name              = var.vm_name
+  vm_size              = var.vm_size
+  vm_username          = var.vm_username
+  vm_password          = var.vm_password
+  vm_image_publisher   = var.vm_image_publisher
+  vm_image_offer       = var.vm_image_offer
+  vm_image_sku         = var.vm_image_sku
+  vm_image_version     = var.vm_image_version
+  vm_os_disk_strg_type = var.vm_os_disk_strg_type
+  vm_os_disk_caching   = var.vm_os_disk_caching
+  vm_subnetid          = module.vnet01.subnet_Id[3]
+}
---
+++ terraform-resources/variables.tf
@@ -0,0 +1,30 @@
+variable "subscription_id" { type = string }
+variable "location" { type = string }
+variable "rg_Name" { type = string }
+
+# Backend storage
+variable "backend_rg_name" { type = string }
+variable "backend_sa_name" { type = string }
+variable "backend_container_name" { type = string }
+
+# VNET module
+variable "vnet_Name" { type = string }
+variable "vnet_Address" { type = string }
+variable "subnet_NameList" { type = list(string) }
+variable "subnet_AddressList" { type = list(string) }
+
+# Windows VM module
+variable "vm_pip" { type = string }
+variable "pip_allocation" { type = string }
+variable "vm_nic" { type = string }
+variable "ip_configuration" { type = string }
+variable "vm_name" { type = string }
+variable "vm_size" { type = string }
+variable "vm_username" { type = string }
+variable "vm_password" { type = string }
+variable "vm_image_publisher" { type = string }
+variable "vm_image_offer" { type = string }
+variable "vm_image_sku" { type = string }
+variable "vm_image_version" { type = string }
+variable "vm_os_disk_strg_type" { type = string }
+variable "vm_os_disk_caching" { type = string }
