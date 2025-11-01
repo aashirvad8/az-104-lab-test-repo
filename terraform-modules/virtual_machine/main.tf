e62ff93b54e1e86812fa0dbfca1a8690489de076
aashirvad8 <aashirvad8@gmail.com>
2025-11-01 12:15:33 +0530

    Update main.tf

--- terraform-modules/virtual_machine/main.tf
+++ terraform-modules/virtual_machine/main.tf
@@ -1,73 +1,43 @@
-variable "vm_name_prefix" {
-  type        = string
-  description = "Prefix used for VM name"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region for the VM"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Resource Group where the VM will be deployed"
-}
-
-variable "subnet_id" {
-  type        = string
-  description = "ID of the subnet to attach the VM NIC"
-}
-
-variable "admin_username" {
-  type        = string
-  default     = "azureuser"
-  description = "Administrator username for the VM"
-}
-
-variable "admin_password" {
-  type        = string
-  default     = "Password123!" # Replace or use secure secrets
-  description = "Administrator password for the VM"
-}
-
-provider "azurerm" {
-  features {}
-}
-
-# Network Interface
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name_prefix}-nic"
+resource "azurerm_public_ip" "vm_pip" {
+  name                = var.vm_pip
+  resource_group_name = var.rg_Name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  allocation_method   = var.pip_allocation
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = var.vm_nic
+  resource_group_name = var.rg_Name
+  location            = var.location
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
+    name                          = var.ip_configuration
+    subnet_id                     = var.vm_subnetid
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
   }
 }
 
-# Windows Virtual Machine
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "${var.vm_name_prefix}-vm"
-  resource_group_name = var.resource_group_name
+resource "azurerm_windows_virtual_machine" "winvm" {
+  name                = var.vm_name
+  resource_group_name = var.rg_Name
   location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+  size                = var.vm_size
+  admin_username      = var.vm_username
+  admin_password      = var.vm_password
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
 
   source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
+    publisher = var.vm_image_publisher
+    offer     = var.vm_image_offer
+    sku       = var.vm_image_sku
+    version   = var.vm_image_version
+  }
+
+  os_disk {
+    storage_account_type = var.vm_os_disk_strg_type
+    caching              = var.vm_os_disk_caching
   }
 }
