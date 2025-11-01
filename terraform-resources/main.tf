1d761c1ed6f3edf16c88cec4521a0172b69d8cb3
aashirvad8 <aashirvad8@gmail.com>
2025-11-01 10:46:10 +0530

    Update main.tf

--- terraform-resources/main.tf
+++ terraform-resources/main.tf
@@ -2,15 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
+      version = "~> 3.0"
     }
   }
 
+  required_version = ">= 1.3.0"
+
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"           # Make sure this RG exists for backend
-    storage_account_name = "tfstatestgacfortesting"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name   = "tfstate-rg"                      # must exist beforehand
+    storage_account_name  = "tfstatestgacfortesting"         # must exist beforehand
+    container_name        = "tfstate"
+    key                   = "${var.email_prefix}-terraform.tfstate"
   }
 }
 
@@ -18,54 +20,44 @@ provider "azurerm" {
   features {}
 }
 
-# ---------------------------
 # Variables
-# ---------------------------
-variable "rg_Name" {
+variable "email_prefix" {
   type        = string
-  description = "Resource Group Name"
+  description = "Prefix used for resource names"
 }
 
 variable "location" {
   type        = string
-  description = "Azure Region"
+  default     = "eastus"
 }
 
-variable "vnet_Name" {
-  type        = string
-  description = "Virtual Network Name"
-}
-
-variable "email_prefix" {
-  type        = string
-  description = "Email prefix for resources"
-}
-
-# ---------------------------
 # Resource Group
-# ---------------------------
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg_Name
+  name     = "${var.email_prefix}-rg"
   location = var.location
 }
 
-# ---------------------------
 # Virtual Network
-# ---------------------------
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_Name
+  name                = "${var.email_prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
 }
 
-# ---------------------------
-# Windows VM module
-# ---------------------------
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.email_prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Optional: Windows VM Module (example)
 module "winvm" {
   source              = "../terraform-modules/virtual_machine"
-  rg_name             = azurerm_resource_group.rg.name
+  vm_name_prefix      = var.email_prefix
   location            = azurerm_resource_group.rg.location
-  vnet_name           = azurerm_virtual_network.vnet.name
-  email_prefix        = var.email_prefix
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet.id
 }
