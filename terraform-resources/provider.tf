ee85e3621cee20698559e78457e53365290b050f
aashirvad8 <aashirvad8@gmail.com>
2025-11-01 11:52:35 +0530

    Update provider.tf

--- terraform-resources/provider.tf
+++ terraform-resources/provider.tf
@@ -1,13 +1,19 @@
-
 terraform {
+  required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestgacfortesting"
+    container_name       = "tfstate"
+    key                  = "${var.email_prefix}-terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.70.0"
     }
   }
-
-  backend "azurerm" {}
 }
 
 provider "azurerm" {
