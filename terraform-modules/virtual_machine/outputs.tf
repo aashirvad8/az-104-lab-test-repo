617eb0741b8a96cde6aa686c2a597c059714dd3e
aashirvad8 <aashirvad8@gmail.com>
2025-11-01 12:15:47 +0530

    Update outputs.tf

--- terraform-modules/virtual_machine/outputs.tf
+++ terraform-modules/virtual_machine/outputs.tf
@@ -1,14 +1,3 @@
-
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_pip.ip_address
-}
-
-output "vm_name" {
-  description = "Name of the created Windows VM"
-  value       = azurerm_windows_virtual_machine.vm.name
-}
-
-output "vm_private_ip" {
-  description = "Private IP address of the VM"
-  value       = azurerm_network_interface.nic.private_ip_address
 }
