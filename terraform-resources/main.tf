9ba01ed7e5da01568614e3e06fc3b8cb82c76e69
aashirvad8 <aashirvad8@gmail.com>
2025-11-01 10:29:42 +0530

    Update main.tf

--- terraform-resources/main.tf
+++ terraform-resources/main.tf
@@ -1,71 +1,68 @@
-###############################################
-# Variables from Workflow
-###############################################
-variable "email_prefix" {
-  description = "Prefix derived from user email (before @)"
-  type        = string
-}
+##############################################
+# Resource Group (Create if not exists)
+##############################################
 
-###############################################
-# Try to find existing Resource Group (optional)
-###############################################
 data "azurerm_resource_group" "existing" {
-  count = 1
-  name  = var.rg_Name
+  name = "${var.email_prefix}-rg"
 }
 
-# We’ll use a try() expression later so Terraform doesn’t fail
-# if the RG doesn’t exist yet.
-
-###############################################
-# Create Resource Group if not found
-###############################################
 resource "azurerm_resource_group" "rg" {
-  count    = try(data.azurerm_resource_group.existing[0].name, null) == null ? 1 : 0
-  name     = var.rg_Name
+  count    = length(data.azurerm_resource_group.existing.name) > 0 ? 0 : 1
+  name     = "${var.email_prefix}-rg"
   location = var.location
 }
 
 locals {
-  effective_rg_name = try(
-    data.azurerm_resource_group.existing[0].name,
-    azurerm_resource_group.rg[0].name
-  )
+  rg_name = length(data.azurerm_resource_group.existing.name) > 0 ? data.azurerm_resource_group.existing.name : azurerm_resource_group.rg[0].name
 }
 
-###############################################
-# VNET & SUBNET Module
-###############################################
+##############################################
+# VNET & Subnets
+##############################################
+
 module "vnet01" {
   source             = "../terraform-modules/network"
   vnet_Name          = "${var.email_prefix}-vnet"
-  rg_Name            = local.effective_rg_name
+  rg_Name            = local.rg_name
   location           = var.location
-  vnet_Address       = var.vnet_Address
-  subnet_NameList    = var.subnet_NameList
-  subnet_AddressList = var.subnet_AddressList
+  vnet_Address       = "178.29.192.0/20"
+  subnet_NameList    = ["${var.email_prefix}-snet-aks", "${var.email_prefix}-snet-agw", "${var.email_prefix}-snet-shared", "${var.email_prefix}-snet-vm", "GatewaySubnet"]
+  subnet_AddressList = ["178.29.192.0/26", "178.29.192.64/26", "178.29.192.128/26", "178.29.192.192/26", "178.29.193.0/26"]
 }
 
-###############################################
-# Azure Windows Virtual Machine Module
-###############################################
+##############################################
+# Windows VM Deployment
+##############################################
+
 module "winvm" {
   source               = "../terraform-modules/virtual_machine"
-  vm_pip               = "${var.email_prefix}-public-ip"
-  rg_Name              = local.effective_rg_name
+  vm_pip               = "${var.email_prefix}-vm-pip"
+  rg_Name              = local.rg_name
   location             = var.location
-  pip_allocation       = var.pip_allocation
-  vm_nic               = "${var.email_prefix}-nic"
-  ip_configuration     = var.ip_configuration
+  pip_allocation       = "Static"
+  vm_nic               = "${var.email_prefix}-vm-nic"
+  ip_configuration     = "ipconfig1"
   vm_name              = "${var.email_prefix}-vm"
-  vm_size              = var.vm_size
-  vm_username          = var.vm_username
-  vm_password          = var.vm_password
-  vm_image_publisher   = var.vm_image_publisher
-  vm_image_offer       = var.vm_image_offer
-  vm_image_sku         = var.vm_image_sku
-  vm_image_version     = var.vm_image_version
-  vm_os_disk_strg_type = var.vm_os_disk_strg_type
-  vm_os_disk_caching   = var.vm_os_disk_caching
+  vm_size              = "Standard_B2s"
+  vm_username          = "AdminUser"
+  vm_password          = "Admin@12356"
+  vm_image_publisher   = "MicrosoftWindowsServer"
+  vm_image_offer       = "WindowsServer"
+  vm_image_sku         = "2016-Datacenter"
+  vm_image_version     = "latest"
+  vm_os_disk_strg_type = "Standard_LRS"
+  vm_os_disk_caching   = "ReadWrite"
   vm_subnetid          = module.vnet01.subnet_Id[3]
+}
+
+##############################################
+# Outputs
+##############################################
+
+output "rg_name" {
+  value = local.rg_name
+}
+
+output "vm_public_ip" {
+  value = module.winvm.vm_public_ip
 }
